dba3c29316907affe625f08483a809692c22332a
Martin Korinek <work@korinek.cc>
2021-12-03 20:51:25 +0100

    working day 2 part 1

--- aoc_02.py
+++ aoc_02.py
@@ -10,3 +10,28 @@
 # easy like brute force parsing
 # switch case?
 # sub class
+
+
+def pilot_sub(input):
+    horz_pos = 0
+    depth = 0
+    with open(input, 'r') as subctrl:
+        ctrl_list = subctrl.readlines()
+        print(ctrl_list)
+        for ctrl in ctrl_list:
+            cmd,value = ctrl.split()
+            value = int(value)
+            if cmd == 'forward':
+                horz_pos += value
+            elif cmd == 'down':
+                depth += value
+            elif cmd == 'up':
+                depth -= value
+            #print(horz_pos, depth, horz_pos*depth)
+            
+
+    return horz_pos*depth
+
+
+print(pilot_sub('aoc_02_example.txt'))
+print(pilot_sub('aoc_02_input.txt'))
