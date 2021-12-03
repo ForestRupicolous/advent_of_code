5714204b90b426aadf64040e4abc609954da28c1
Korinek Martin (BEG/ENC6-Wi) <mak5wi@bosch.com>
2021-12-03 14:30:42 +0100

    Working 2nd part code

--- aoc_01.py
+++ aoc_01.py
@@ -9,20 +9,35 @@
 
 # return number of depth increases
 
-
-
-
 def aoc_count_depth_increase(aoc_input):
     prev_depth = 0
     cnt = -1
-    with open(aoc_input) as input:
+    with open(aoc_input, 'r') as input:
         for depth in input.readlines():
             if int(depth) > prev_depth:
                 cnt+=1
             prev_depth = int(depth)
     return cnt
 
+#2nd challenge - better code as integers are converted in the beginning
+def aoc_count_depth_sum_increase(aoc_input, winsize=3):
+
+    prev_depth_sum = 0
+    cnt = -1
+    with open(aoc_input, 'r') as input:
+        depths = list(map(int,input.readlines()))
+        print(depths)
+        for i in range(len(depths) - winsize + 1):
+            window = depths[i: i + winsize]
+            depth_sum = sum(window)
+            print(window, sum(window))
+            if depth_sum > prev_depth_sum:
+                cnt+=1
+            prev_depth_sum = depth_sum
+    return cnt
+
 print("Hello World!")
 print(aoc_count_depth_increase('aoc_01_example.txt'))
 print(aoc_count_depth_increase('aoc_01_input.txt'))
+print(aoc_count_depth_sum_increase('aoc_01_input.txt'))
 
