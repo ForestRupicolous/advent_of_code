0be1de080638cdf2046239411843488ed7414084
Korinek Martin (BEG/ENC6-Wi) <mak5wi@bosch.com>
2021-12-03 12:08:08 +0100

    solution day 01 part 1

--- aoc_01.py
+++ aoc_01.py
@@ -2,28 +2,27 @@
 # aoc_01.py
 # Advent of code:
 # https://adventofcode.com/2021/day/1
+# https://adventofcode.com/2021/day/1#part2
 
 # download input data (optional, for future use)
 # Count depth increase (if current num is > last: ++)
 
 # return number of depth increases
-import urllib.request
 
 
 
 
 def aoc_count_depth_increase(aoc_input):
-    proxies = { "http": "http://127.0.0.1:3128/",
-            "https": "http://127.0.0.1:3128/"}
-    proxy = urllib.request.ProxyHandler(proxies)
-    opener = urllib.request.build_opener(proxy)
-    urllib.request.install_opener(opener)
-    with urllib.request.urlopen(aoc_input) as input:
-    #input = urllib.request.urlopen(aoc_input)
-        print(input)
-    return 0
+    prev_depth = 0
+    cnt = -1
+    with open(aoc_input) as input:
+        for depth in input.readlines():
+            if int(depth) > prev_depth:
+                cnt+=1
+            prev_depth = int(depth)
+    return cnt
 
 print("Hello World!")
-aoc_count_depth_increase('https://adventofcode.com/2021/day/1/input')
+print(aoc_count_depth_increase('aoc_01_example.txt'))
+print(aoc_count_depth_increase('aoc_01_input.txt'))
 
- input =
