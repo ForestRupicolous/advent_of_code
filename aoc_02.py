3c161eb5a85bb94eac5da286b5eddd0d527ab18b
Korinek Martin (BEG/ENC6-Wi) <mak5wi@bosch.com>
2021-12-03 14:37:52 +0100

    Started day 2

---
+++ aoc_02.py
@@ -0,0 +1,12 @@
+#! python3
+# aoc_02.py
+# Advent of code:
+# https://adventofcode.com/2021/day/2
+# https://adventofcode.com/2021/day/2#part2
+
+
+# read in sub commands
+# pretty (like calculator)
+# easy like brute force parsing
+# switch case?
+# sub class
