cc602ca168e4ca1179b898eac18bb920e4a7da54
Martin Korinek <work@korinek.cc>
2021-12-04 20:13:29 +0100

    added remove num and check row function

--- aoc_04.py
+++ aoc_04.py
@@ -12,8 +12,26 @@ class BBoard:
         self.board = [[0 for i in range(c)] for j in range(r)]
 
     def add_row(self,row,i):
-        self.board[i][:]=row
+        self.board[i][:]=list(map(int,row))
         print(self.board)
+    
+    def remove_num(self, num):
+        for i in range(len(self.board)):
+            for l in range(len(self.board[i])):                
+                if self.board[i][l] is num:
+                    self.board[i][l] = -1
+
+    def check_rows(self):
+        for row in self.board:
+            if sum(row) == -5:
+                return True
+        return False
+    
+    def check_columns(self):
+        for row in self.board:
+            if sum(row) == -5:
+                return True
+        return False
 
 def play_bingo(input, called_nums ):
     bingoboards = list()
@@ -31,7 +49,15 @@ def play_bingo(input, called_nums ):
                 bingoboards.append(cur_board)
                 i=0
                 cur_board = BBoard(5,5)
-                print('########################')       
+                print('########################')
+
+    bingoboards[1].remove_num(3)
+    bingoboards[1].remove_num(0)
+    bingoboards[1].remove_num(15)
+    bingoboards[1].remove_num(2)
+    bingoboards[1].remove_num(22)
+    print(bingoboards[1].check_rows())
+    print(bingoboards[1].board)
     return 0
 
 example_nums = (7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1)
