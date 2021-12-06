fe29f9134c6afc2105920be59939d52a6d7d06ef
Korinek Martin (BEG/ENC6-Wi) <mak5wi@bosch.com>
2021-12-06 14:22:36 +0100

    prep day7

---
+++ aoc_07.py
@@ -0,0 +1,23 @@
+#! python3
+# aoc_07.py
+# Advent of code:
+# https://adventofcode.com/2021/day/7
+# https://adventofcode.com/2021/day/7#part2
+#
+def part_one(input):
+    return 0
+
+def part_two(input):
+    return 0
+
+
+
+if __name__ == "__main__":
+    example_path = "./aoc_07_example.txt"
+    input_path = "./aoc_07_input.txt"   
+    print("---Part One---")
+    print(part_one(example_path))
+    print(part_one(input_path))
+
+    print("---Part Two---")
+    print(part_two(input_path))
