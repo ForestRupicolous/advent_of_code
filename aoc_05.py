dc7f58a91bdd00e80c6a2217fa80e4274a5c0eec
Martin Korinek <work@korinek.cc>
2021-12-06 08:02:47 +0100

    wip

--- aoc_05.py
+++ aoc_05.py
@@ -24,8 +24,8 @@ def check_line(x1,y1,x2,y2):
 
 
 
-def find_vents(input):
-    mymap = ventmap(1000,1000)
+def find_vents(input,x,y):
+    mymap = ventmap(x,y)
     with open(input, 'r') as line_list:
         for line in line_list.readlines():
             lstart, lend = line.split(' -> ')
@@ -43,15 +43,34 @@ def find_vents(input):
                 for i in range(x1,x2+1):
                     for l in range(y1,y2+1):
                         mymap.map[l][i]+=1
-            #else:
-                #print("Line diag:", x1,y1,x2,y2)
+            else:
+                
+                if x1>x2 or y1>y2:
+                    x1,x2 = x2,x1
+                    y1,y2 = y2,y1
+                print("Line diag:", x1,y1,x2,y2)
+                #print("Line OK:", x1,y1,x2,y2)
+
+                #diag variants, diag up:
+                if x1 < x2 and y1 < y2:
+                    print("Diag down")
+                    for i  in range(0,x2-x1):
+                        mymap.map[x1+i][y1+i]+=1
+                #diag down:
+                else:
+                    print("Diag up")
+
+
+
+
     danger_points = 0
     for line in mymap.map:
+        print(line)
         for field in line:
             if field > 1:
                 danger_points +=1
 
     return danger_points
 
-print(find_vents("aoc_05_example.txt"))
-print(find_vents("aoc_05_input.txt"))
+print(find_vents("aoc_05_example.txt",10,10))
+#print(find_vents("aoc_05_input.txt",1000,1000))
