750eb22a23c983e86ea1ba2ad1f7191470cad213
Korinek Martin (BEG/ENC6-Wi) <mak5wi@bosch.com>
2021-12-06 08:24:09 +0100

    day 5 part 2 fin

--- aoc_05.py
+++ aoc_05.py
@@ -8,21 +8,16 @@
 
 def hello_world():
     return 'hello world'
+    
 
 class ventmap:
     def __init__(self, xmax, ymax):
         self.xmax = xmax
         self.ymax = ymax
         self.map = [[0 for i in range(xmax)] for j in range(ymax)]
-
-    
-
     
 def check_line(x1,y1,x2,y2):
     return x1 == x2 or y1 == y2
-
-
-
 
 def find_vents(input,x,y):
     mymap = ventmap(x,y)
@@ -39,30 +34,26 @@ def find_vents(input,x,y):
                 if x1>x2 or y1 >y2:
                     x1,x2 = x2,x1
                     y1,y2 = y2,y1
-                #print("Line OK:", x1,y1,x2,y2)
                 for i in range(x1,x2+1):
                     for l in range(y1,y2+1):
                         mymap.map[l][i]+=1
             else:
-                print("Line:", x1,y1,x2,y2)
                 if x1>x2:
                     x1,x2 = x2,x1
                     y1,y2 = y2,y1
-                print("Line diag:", x1,y1,x2,y2)
 
                 if x1 < x2 and y1 < y2:
-                    print("Diag down")
+                    #print("Diag down")
                     for i in range(0,x2-x1+1):
                         mymap.map[y1+i][x1+i] += 1
                 #diag down:
                 else:
-                    print("Diag up")
+                    #print("Diag up")
                     for i in range(0,x2-x1+1):
                         mymap.map[y1-i][x1+i] += 1                   
 
     danger_points = 0
     for line in mymap.map:
-        print(line)
         for field in line:
             if field > 1:
                 danger_points +=1
@@ -70,4 +61,4 @@ def find_vents(input,x,y):
     return danger_points
 
 print(find_vents("aoc_05_example.txt",10,10))
-#print(find_vents("aoc_05_input.txt",1000,1000))
+print(find_vents("aoc_05_input.txt",1000,1000))
