33da57e9f78f71cc6dd46f84b7bc4cc704f6da40
Korinek Martin (BEG/ENC6-Wi) <mak5wi@bosch.com>
2021-12-09 22:03:32 +0100

    Dirty slow solution day 9 part 2

--- aoc_09.py
+++ aoc_09.py
@@ -13,6 +13,7 @@ def part_one(input) -> int:
     j = 1
     with open(input, 'r') as digits:
         lines = digits.readlines()
+        dmap = [[99 for i in range(102)] for j in range(len(lines)+2)]       
         for line in lines:
             d.append(line.strip())
         dmap = [[99 for i in range(len(d[0])+2)] for j in range(len(lines)+2)]  
@@ -38,6 +39,7 @@ def part_one(input) -> int:
     return risk
 
 def part_two(input) -> int:
+    import math
     #idea, for each field increase value of lower neigthbor by own value
     d = list()
     lowpoints = dict ()
@@ -47,7 +49,7 @@ def part_two(input) -> int:
     j = 1
     with open(input, 'r') as digits:
         lines = digits.readlines()
-        dmap = [[99 for i in range(1000)] for j in range(1000)]  
+        dmap = [[99 for i in range(102)] for j in range(len(lines)+2)]  
         for line in lines:
             d.append(line.strip())
         dmap = [[99 for x in range(len(d[0])+2)] for y in range(len(lines)+2)]
@@ -58,9 +60,8 @@ def part_two(input) -> int:
                 i+=1
             i = 1
             j +=1
-        #for times in range(1000):
+        for times in range(1000):
             for i in range(1,len(d[0])+1):
-                col_sum = 0
                 for l in range(1,len(lines)+1):
                     value = dmap[l][i]
                     if value != 9:
@@ -68,29 +69,31 @@ def part_two(input) -> int:
                         if dmap[l-1][i] < value:
                             basins[l-1][i] += basins[l][i]
                             basins[l][i] = 0
-                            break
-                        if dmap[l+1][i] < value:
+                        elif dmap[l+1][i] < value:
                             basins[l+1][i] += basins[l][i]
                             basins[l][i] = 0
-                            break
-                        if dmap[l][i-1] < value:
+                        elif dmap[l][i-1] < value:
                             basins[l][i-1] += basins[l][i]
-                            basins[l][i] = 0
-                            break
-                        if dmap[l][i+1] < value:
+                            basins[l][i] = 0                         
+                        elif dmap[l][i+1] < value:
                             basins[l][i+1] += basins[l][i]
                             basins[l][i] = 0
-                            break
+    all_basins = [ item for innerlist in basins for item in innerlist ]                     
+    all_basins.sort()
     print(basins)
-    return risk
+    basins.sort()
+    print(basins)
+    print(all_basins)
+    print(all_basins[-3:])
+    return math.prod(all_basins[-3:])
 
 if __name__ == "__main__":
     example_path = "./aoc_09_example.txt"
     input_path = "./aoc_09_input.txt"   
     print("---Part One---")
-    print(part_one(example_path))
+  #  print(part_one(example_path))
    # print(part_one(input_path))
 
     print("---Part Two---")
-    print(part_two(example_path))
-   # print(part_two(input_path))
+  #  print(part_two(example_path))
+    print(part_two(input_path))
