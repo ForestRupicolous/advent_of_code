b3a17364a0e2c7be63482512fbb2eade34e76e03
Martin Korinek <work@korinek.cc>
2021-12-12 22:24:37 +0100

    wip

--- aoc_11.py
+++ aoc_11.py
@@ -5,10 +5,15 @@
 # https://adventofcode.com/2021/day/11#part2
 #
 def part_one(input) -> int:
+    octo = []
+    flashes = 0
     with open(input, 'r') as inp:
-        #rows = [digit for line in inp.readlines() for digit in line.strip()]
-        #when the digits are not stored as single elements it is directly possible to use 2d coordinate
-        octo = [line.strip() for line in inp.readlines()]
+
+        #WHY thsi works and 
+        # rows.append([int(x) for line in inp.readlines() for x in list(line.strip())])
+        #NOT?
+        for line in inp.readlines():
+            octo.append([int(x) for x in list(line.strip())])
         #delta colum and delta row
         #up, upright,right,downright, down,downleft,left,upleft
         DR = [-1,-1,0,1,1,1,0,-1]
@@ -16,27 +21,35 @@ def part_one(input) -> int:
         R = len(octo)
         C = len(octo[0])
         flashed = False
-        for r in range(R):
-            for c in range(C):
-                octo[r][c] += 1
+        once = True
+        for i in range(10):
+            for r in range(R):
+                for c in range(C):
+                    octo[r][c] += 1
 
-        for r in range(R):
-            for c in range(C):
-                if octo[r][c] > 9:
-                    flashed = True
-                    for d in range(8): #8 directions
-                        rr = r+DR[d]
-                        cc = c+DC[d]
-                        #test for neighbor in  field:
-                        if 0 < rr < R and 0 < cc < C:
-                            octo[rr][cc] +=1
+            while flashed or once:
+                once = False
+                for r in range(R):
+                    for c in range(C):
+                        if octo[r][c] > 9:
+                            flashed = True
+                            flashes += 1
+                            octo[r][c] = 0
+                            for d in range(8): #8 directions
+                                rr = r+DR[d]
+                                cc = c+DC[d]
+                                #test for neighbor in  field:
+                                if 0 < rr < R and 0 < cc < C:
+                                    octo[rr][cc] +=1
 
         #rows = inp.readlines()
         print(octo)
-
+        print(octo[9][9])
 
    # elf.board = [[0 for i in range(c)] for j in range(r)]
-    return 0
+    return flashes
+
+
 
 def part_two(input) -> int:
     return 0
