a3701733bdac80e74a31710a6c3960657c06dd84
Korinek Martin (BEG/ENC6-Wi) <mak5wi@bosch.com>
2021-12-13 09:40:07 +0100

    wip

--- aoc_13.py
+++ aoc_13.py
@@ -8,38 +8,42 @@
 def part_one(input) -> int:
     nmap =[]
     ymap = []
+    coords = []
     with open(input, 'r') as inp:
         lines = inp.readlines()
-        R = 15#len(lines)
-        C = 11#len(lines[0])
+
+        for line in lines:
+            line.strip()
+            coords.append([int(line.split(',')[1]),int(line.split(',')[0])])
+
+        R = 655*2+1#max([_[0] for _ in coords])+1
+        C = 447*2+1#max([_[1] for _ in coords])+1
+        print('R:',R,'C:',C)
         dmap = [[0 for columns in range(C)] for rows in range(R)]
-        
-        print(dmap)
-        #x-fold
-        x = 6
-        for row in dmap:
-            nmap.append([x + y for x, y in zip(row[:x-1], row[x:])])
-            print(nmap)
-
-        #y-fold
-        yfold = 7
-        for y in range(yfold):
-            ymap.append([x + y for x, y in zip(dmap[y][:], dmap[-y][:])])
-    print(ymap)
+        for line in coords:
+            dmap[line[1]][line[0]] = 1
+        #print(dmap)
+        dmap = xfold(dmap,655)  
 
 
-    return 0
+   #print('####################')
+    #print(dmap)
+   # dmap = xfold(dmap,5)
+    #print('####################')
+   # print(dmap)
 
-def yfold(map,yf):
+    return sum([sum(i) for i in dmap])
+
+def yfold(m,yf):
     nmap = []
     for y in range(yf):
-        nmap.append([x + y for x, y in zip(map[y][:], map[-y][:])])
+        nmap.append([x or y for x, y in zip(m[:][y], m[:][-y-1])])
     return nmap
 
-def xfold(map,xf):
+def xfold(m,xf):
     nmap =[]
-    for row in map:
-        nmap.append([x + y for x, y in zip(row[:xf-1], row[xf:])])
+    for row in m:
+        nmap.append([x or y for x, y in zip(row[:xf], row[:xf:-1])])#list reverse [::-1]
     return nmap
 
 
@@ -52,8 +56,23 @@ if __name__ == "__main__":
     example_path = "./aoc_13_example.txt"
     input_path = "./aoc_13_input.txt"   
     print("---Part One---")
-    print(part_one(example_path))
- #   print(part_one(input_path))
+ #   print(part_one(example_path))
+    print(part_one(input_path))
 
     print("---Part Two---")
  #   print(part_two(input_path))
+
+
+ 
+#fold along x=655
+#fold along y=447
+#fold along x=327
+#fold along y=223
+#fold along x=163
+#fold along y=111
+#fold along x=81
+#fold along y=55
+#fold along x=40
+#fold along y=27
+#fold along y=13
+#fold along y=6
--- readme.md
+++ readme.md
@@ -20,7 +20,7 @@ Task                                                              | Python Solut
 [Day 10](http://adventofcode.com/2021/day/10)                     | [aoc_10.py](aoc_10.py) |
 [Day 11](http://adventofcode.com/2021/day/11)                     | [aoc_11.py](aoc_11.py) |
 [Day 12](http://adventofcode.com/2021/day/12)                     | [aoc_12.py](aoc_12.py) |
-[Day 13](http://adventofcode.com/2021/day/13)                     | [aoc_13.py](aoc_13.py) |
+[Day 13](http://adventofcode.com/2021/day/13)                     | [aoc_13.py](aoc_13.py) |List reverse by [::-1]
 [Day 14](http://adventofcode.com/2021/day/14)                     | [aoc_14.py](aoc_14.py) |
 [Day 15](http://adventofcode.com/2021/day/15)                     | [aoc_15.py](aoc_15.py) |
 [Day 16](http://adventofcode.com/2021/day/16)                     | [aoc_16.py](aoc_16.py) |
