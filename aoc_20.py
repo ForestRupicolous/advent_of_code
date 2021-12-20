59145f60f712aed11b6c16cce3c6eda7e053001f
Martin Korinek <work@korinek.cc>
2021-12-20 20:59:22 +0100

    wip - copy inital array into big picture or figure out alternative method for infinite

--- aoc_20.py
+++ aoc_20.py
@@ -4,15 +4,35 @@
 # https://adventofcode.com/2021/day/20
 # https://adventofcode.com/2021/day/20#part2
 #
+from aoc_19 import visualize
+
+
 def part_one(input) -> int:
     with open(input, 'r') as f:
         lines = f.readlines()
         algo = lines[0].strip()
         data = [[int(x) for x in line.strip()] for line in lines[2:]]
-        print(algo)
-        print(algo[0], algo[-1])
-        print(data)
-    return 0
+
+        NR = len(data)
+        NC = len(data[0])
+        pic = [[0 for i in range(-3*NR,5*NR)] for j in range(-3*NC,5*NC)]  
+        for step in range(2):
+            for r in range(-3*NR,3*NR):
+                for c in range(-3*NC,3*NC):
+                    val = ''
+                    for dr in range(-1,2):
+                        for dc in range(-1,2):
+                            if 0 <= r+dr < NR and 0 <= c+dc < NC:
+                                if step == 0:
+                                    val += str(data[r+dr][c+dc])
+                                else:
+                                    val += str(pic[2*NR+r+dr][2*NC+c+dc])
+                            else:
+                                val += '0'
+
+                    pic[2*NR+r][2*NC+c]=algo[int(val,2)]
+
+    return sum([r.count('1') for r in pic])
 
 def part_two(input) -> int:
     with open(input, 'r') as f:
