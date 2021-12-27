099c153d8c1eb4f9dbc7d0baa235f30f2281cd22
Martin Korinek <work@korinek.cc>
2021-12-27 12:11:52 +0100

    day 25 part 1 fin

--- aoc_25.py
+++ aoc_25.py
@@ -1,42 +1,50 @@
 #! python3
 # aoc_25.py
 # Advent of code:
-# https://adventofcode.com/2021/day/7
-# https://adventofcode.com/2021/day/7#part2
+# https://adventofcode.com/2021/day/25
+# https://adventofcode.com/2021/day/25#part2
 #
+from copy import deepcopy
+
 def part_one(input) -> int:
     with open(input, 'r') as f:
         data = [[x for x in line.strip()] for line in f.readlines()]
-        print(data)
+        new_data = deepcopy(data)
+        #print(data)
         rounds = 0
         has_moved = 1
         NC = len(data[0])
         NR = len(data)
-
+        #print(id(data[0]),id(new_data[0]))
         while has_moved:
         # how to fullfill the only move when it was free before rule and avoid moving it twice?
             has_moved = 0
-            for r in range(NR-1,-1,-1):
-                for c in range(NC-2,-1,-1):
+            for r in range(NR):
+                for c in range(NC-1):
                     if data[r][c] == '>' and data[r][c+1] =='.':
-                        data[r][c] = '.'
-                        data[r][c+1] = '>'
+                        new_data[r][c] = '.'
+                        new_data[r][c+1] = '>'
                         has_moved = 1
                 if data[r][NC-1] == '>' and data[r][0] =='.':
-                    data[r][NC-1] = '.'
-                    data[r][0] = '>'
+                    new_data[r][NC-1] = '.'
+                    new_data[r][0] = '>'
                     has_moved = 1
-            for c in range(NC-1,-1,-1):
-                for r in range(NR-2,0-1,-1):
+            #print(*(' '.join(row) for row in data), sep='\n')
+            #print('------------------------------------')
+            data = deepcopy(new_data)
+            #print(*(' '.join(row) for row in data), sep='\n')
+            #print('################################################################')
+            for c in range(NC):
+                for r in range(NR-1):
                     if data[r][c] == 'v' and data[r+1][c] =='.':
-                        data[r][c] = '.'
-                        data[r+1][c] = 'v'
+                        new_data[r][c] = '.'
+                        new_data[r+1][c] = 'v'
                         has_moved = 1
                 if data[NR-1][c] == 'v' and data[0][c] =='.':
-                    data[NR-1][c] = '.'
-                    data[0][c] = 'v'
+                    new_data[NR-1][c] = '.'
+                    new_data[0][c] = 'v'
                     has_moved = 1
-
+            data = deepcopy(new_data)
 
             rounds += 1
         print('####')
@@ -55,7 +63,7 @@ if __name__ == "__main__":
     input_path = "./aoc_25_input.txt"   
     print("---Part One---")
     print(part_one(example_path))
-   # print(part_one(input_path))
+    print(part_one(input_path))
 
     print("---Part Two---")
    # print(part_two(input_path))
