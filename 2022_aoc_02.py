3c4073b3c794e773425af49f07c6efc11d07fc89
Martin Korinek <m.korinek@auersignal.com>
2022-12-02 09:05:00 +0100

    Finished day 2

--- 2022_aoc_02.py
+++ 2022_aoc_02.py
@@ -5,31 +5,39 @@
 # https://adventofcode.com/2022/day/2#part2
 #
 # %%
-
-rps_trsn = dict('A X':0,'A Y':0,'A Z':0,'B X':0,'B Y':0,'B Z':0, 'C X':0, 'C Y':0, 'C Z':0)
+#X Rock 1
+#Y Paper 2
+#Z Scissor 3
+#
+rps_trsn_part1 = {'A X':4,'A Y':8,'A Z':3,'B X':1,'B Y':5,'B Z':9, 'C X':7, 'C Y':2, 'C Z':6}
+rps_trsn_part2 = {'A X':3,'A Y':4,'A Z':8,'B X':1,'B Y':5,'B Z':9, 'C X':2, 'C Y':6, 'C Z':7}
 # %%
 def part_one(input = "./aoc_02_example.txt") -> int:
-    print("hello world")
+    data = 0
     with open(input, 'r') as f:
-        data = [[int(x) for x in line.strip()] for line in f.readlines()]   
-    return 0
+        for line in f.readlines():
+            data += rps_trsn_part1[line.strip()]
+   
+    return data
 
 # %%
 def part_two(input) -> int:
+    data = 0
     with open(input, 'r') as f:
-        data = [[int(x) for x in line.strip()] for line in f.readlines()]
-    return 0
-
+        for line in f.readlines():
+            data += rps_trsn_part2[line.strip()]
+    
+    return data
 
 # %%
 if __name__ == "__main__":
+
     example_path = "./aoc_02_example.txt"
     input_path = "./aoc_02_input.txt"   
     print("---Part One---")
     print(part_one(example_path))
     print(part_one(input_path))
 
-    #print("---Part Two---")
-    #print(part_two(input_path))
-
-
+    print("---Part Two---")
+    print(part_two(example_path))
+    print(part_two(input_path))
